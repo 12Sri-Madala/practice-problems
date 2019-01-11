fd25e457b84051024c946e8d956b81e7a014ea18
Sridhar Madala <Si.madala19@gmail.com>
2019-01-10 23:22:54 -0800

    finished practice13

---
+++ practice13/practice13.js
@@ -0,0 +1,7 @@
+function mathOperations(num1, num2){
+    console.log(`${num1}+${num2}=${num1 + num2}, ${num1}-${num2}=${num1 - num2}, ${num1}*${num2}=${num1 * num2}, ${num1}/${num2}=${num1 / num2}`)
+    return;
+}
+
+mathOperations(2,5);
+
